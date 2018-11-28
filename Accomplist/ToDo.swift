5b65d9341d8b6f2719c2a4fd88bac4247da289fe
Colin Russell <colinrussell101@gmail.com>
2018-11-27 22:49:20 -0500

    added ToDo.swift

---
+++ Accomplist/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  Accomplist
+//
+//  Created by Colin on 2018-11-27.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var toDoDescription = ""
+    var isCompleted = false
+    var alertDate = Date()
+    var isAlertSet = false
+}
