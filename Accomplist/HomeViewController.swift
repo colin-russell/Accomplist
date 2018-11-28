171ea4b23e2c8ad93b357635dd7a9e48c415389e
Colin Russell <colinrussell101@gmail.com>
2018-11-27 19:47:07 -0500

    fixed menu bug that didn't allow the menuTableView to scroll

--- Accomplist/HomeViewController.swift
+++ Accomplist/HomeViewController.swift
@@ -12,68 +12,90 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     let menuTableView = UITableView()
     let mWMultiplier: CGFloat = 0.4
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var listTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         setupMenu()
     }
     
     func setupMenu() {
-        menuTableView.frame = CGRect(x: -(view.frame.width * mWMultiplier), y: 0, width: view.frame.width * mWMultiplier, height: view.frame.height)
         menuTableView.register(UITableViewCell.self, forCellReuseIdentifier: "listCell")
         menuTableView.dataSource = self
         menuTableView.delegate = self
         menuTableView.backgroundColor = UIColor.darkGray
         menuTableView.layer.borderColor = UIColor.lightGray.cgColor
         menuTableView.layer.borderWidth = 2.0
-        view.addSubview(menuTableView)
+        menuTableView.frame = CGRect(x: 0, y: 0, width: view.frame.width * self.mWMultiplier, height: view.frame.height)
+        
+        view.insertSubview(menuTableView, belowSubview: backView)
+    }
+    
+    func showMenu() {
+        let oldFrame = backView.frame
+        
+        UIView.animate(withDuration: 0.5) {
+            self.backView.frame = CGRect(x: oldFrame.origin.x + (oldFrame.width * self.mWMultiplier), y: oldFrame.origin.y, width: oldFrame.width, height: oldFrame.height)
+        }
+        
+        backView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewTapped(recognizer:))))
+    }
+    
+    func hideMenu() {
+        let oldFrame = backView.frame
+        
+        UIView.animate(withDuration: 0.5) {
+            self.backView.frame = CGRect(x: oldFrame.origin.x - (oldFrame.width * self.mWMultiplier), y: oldFrame.origin.y, width: oldFrame.width, height: oldFrame.height)
+        }
+        for recognizer in backView.gestureRecognizers ?? [] {
+            backView.removeGestureRecognizer(recognizer)
+        }
     }
     
     // MARK: UITableViewDelegate
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if tableView == listTableView  {
+            return 4
+        } else {
+            return 20
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        var cell = UITableViewCell()
+        
+        if tableView == listTableView {
+            cell = listTableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        } else {
+            cell = menuTableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        }
+        
+        cell.textLabel?.text = "Number: \(indexPath)"
+        
+        return cell
         
     }
     
     // MARK: Actions
     
     @objc func viewTapped(recognizer: UITapGestureRecognizer) {
-        let oldFrame = view.frame
-        
-        UIView.animate(withDuration: 0.5) {
-            self.view.frame = CGRect(x: oldFrame.origin.x - (oldFrame.width * self.mWMultiplier), y: oldFrame.origin.y, width: oldFrame.width, height: oldFrame.height)
-        }
-        for recognizer in view.gestureRecognizers ?? [] {
-            view.removeGestureRecognizer(recognizer)
-        }
+        hideMenu()
     }
     
     @IBAction func menuButtonTapped(_ sender: UIButton) {
         print("menu button tapped")
-        let oldFrame = view.frame
-        var newFrame = CGRect()
+        
         
         // show menu
-        if view.frame.origin == CGPoint.zero {
-            newFrame = CGRect(x: oldFrame.origin.x + (oldFrame.width * mWMultiplier), y: oldFrame.origin.y, width: oldFrame.width, height: oldFrame.height)
-            view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewTapped(recognizer:))))
+        if backView.frame.origin == CGPoint.zero {
+            showMenu()
+            
         } else { // hide menu
-            newFrame = CGRect(x: oldFrame.origin.x - (oldFrame.width * mWMultiplier), y: oldFrame.origin.y, width: oldFrame.width, height: oldFrame.height)
-            for recognizer in view.gestureRecognizers ?? [] {
-                view.removeGestureRecognizer(recognizer)
-            }
+            hideMenu()
         }
-        
-        UIView.animate(withDuration: 0.5) {
-            self.view.frame = newFrame
-        }
-        
     }
     
     @IBAction func editButtonTapped(_ sender: UIButton) {
