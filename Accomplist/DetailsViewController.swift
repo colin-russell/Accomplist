ddc576cb56cf268e6a58601750d11f86eba9d7a0
Colin Russell <colinrussell101@gmail.com>
2018-11-27 20:29:34 -0500

    added DetailsViewController

---
+++ Accomplist/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  Accomplist
+//
+//  Created by Colin on 2018-11-27.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
