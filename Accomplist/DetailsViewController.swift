a30d0db5a6b3fde0cfc628ec71630a36b2a8d42c
Colin Russell <colinrussell101@gmail.com>
2018-11-27 21:25:23 -0500

    detailsTextView now dismisses keyboard when the done (return) button is hit

--- Accomplist/DetailsViewController.swift
+++ Accomplist/DetailsViewController.swift
@@ -8,23 +8,37 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
-
+class DetailsViewController: UIViewController, UITextViewDelegate {
+    
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var detailsTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        detailsTextView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: UITextViewDelegate
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
     }
-    */
-
+    
+    // MARK: Actions
+    
+    @IBAction func reminderSwitchToggled(_ sender: UISwitch) {
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
